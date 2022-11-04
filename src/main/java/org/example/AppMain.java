5bbedeb397a1f689c6c3c395ae2d1621e849685f
dermo <dermot.logue@dkit.ie>
2022-11-04 16:15:52 +0000

    Added int type year field to Car class, refactored compareTo() to compare based on year field. Leave as exercise to reorder based on Make (String), then on engineSize(double).

--- src/main/java/org/example/AppMain.java
+++ src/main/java/org/example/AppMain.java
@@ -1,47 +1,52 @@
-package org.example;        // Nov 2021
+package org.example;                           // OOP SD2 Nov 2022
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
+// Demonstrates:
+// Sort an Array of objects (Car objects in this case)
+// Sort a List of objects  (ArrayList of Car objects in this case)
+// Key learning objective is to understand that the class type of objects
+// being sorted (here, a Car class) must implement the Comparable Interface,
+// and hence must provide a compareTo() method to compare cars on some fields.
+//
 public class AppMain
 {
     public static void main(String[] args)
     {
-        Car cars[] = new Car[4];
+        Car cars[] = new Car[4];    // create array for 4 Car objects
         
-        cars[0]= new Car("Toyota","Corolla",1.6);
-        cars[1]= new Car("Daihatsu","Diamond",1.4);
-        cars[2]= new Car("Nissan","Patrol",2.5);
-        cars[3]= new Car("Bugatti","Vyron",4.5);
+        cars[0]= new Car("Toyota","Corolla",2015,1.6);
+        cars[1]= new Car("Daihatsu","Diamond",2008,1.4);
+        cars[2]= new Car("Nissan","Patrol",2022, 2.5);
+        cars[3]= new Car("Bugatti","Vyron",2020, 4.5);
 
-        // Arrays.sort() will sort Car objects based on their natural ordering.
-        // The sort method requires that the objects being sorted must Implement
-        // the Comparable interface, and hence must implement
-        // the compareTo() method.
-        // (Otherwise Compiler objects!)
+        // Arrays.sort() will sort Car objects based on their "natural ordering".
+        // The library supplied sort() method requires that the objects being sorted
+        // must implement the Comparable Interface for the object type (Car here).
+
         Arrays.sort( cars );
-        System.out.println("cars array after Arrays.sort( cars )");
-        
-        for( Car c : cars )
-            System.out.println( c );
-        
+        System.out.println("cars[] array after Arrays.sort( cars )");
+        for( Car car : cars )
+            System.out.println( car );
+
         ArrayList<Car> carList = new ArrayList<Car>();
         
-        carList.add(new Car("Toyota","Corolla",1.6));
-        carList.add(new Car("Daihatsu","Diamond",1.4));
-        carList.add(new Car("Nissan","Patrol",2.5));
-        carList.add(new Car("Bugatti","Vyron",4.5));
+        carList.add(new Car("Toyota","Corolla", 2015,1.6));
+        carList.add(new Car("Daihatsu","Diamond",2008,1.4));
+        carList.add(new Car("Nissan","Patrol",2022,2.5));
+        carList.add(new Car("Bugatti","Vyron",2021,4.5));
         
-        // Collections.sort() will sort Car objects based on their natural ordering
-        // Natural Ordering is dictated by that implemented by the compareTo() method defined
-        // in the Car class.
+        // Collections.sort() will sort Car objects based on their "natural ordering".
+        // There isn't really a "Natural Ordering" for cars, so the designer/programmer
+        // must program in an order by writing the compareTo() method in the Car class.
+
         Collections.sort( carList );
         
         System.out.println("\ncarList ArrayList after Collections.sort( carList )");
-    
-        for( Car c : carList )
-            System.out.println( c );
-        
-        // 2DO: Rewrite code to sort Car objects based on their Make.
+        for( Car car : carList )
+            System.out.println( car );
+
+        //TODO: Rewrite code to sort Car objects based on their Make.
     }  
 }
--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -1,37 +1,59 @@
 package org.example;
 
+// In order to be able to sort an array or list of Car objects,
+// the Arrays.sort() and Collections.sort() methods require that the
+// Car class :
+// 1. implements the Comparable<T> interface, and consequently that
+// 2. the compareTo() method is written in the Car class
+//
+
 public class Car implements Comparable<Car>
 {
     private String make;    //e.g. Toyota
     private String model;   // Corolla
+    private int year;
     private double engineSize;   // e.g.  1.6  litre
 
-    public Car( String make, String model, double engineSize ) {
+    public Car( String make, String model, int year, double engineSize ) {
+        this.year = year;
         this.make = make;
         this.model = model;
         this.engineSize = engineSize;
     }
 
-    // The Car class must implement(define) the compareTo() method
-    // because it implements the Comparable interface.
+    // Once we declare that Car implements the Comparable interface:
     //     "class Car implements Comparable<Car>"
-    // Below we have decided to compare based on engineSize.
-    // This defines the 'natural ordering' for Car objects.
+    // Then we must actually implement (write code for) the
+    // compareTo() method.
+    // Here, we have decided to compare Car objects based on engineSize.
+    // This defines the so-called 'natural ordering' for Car objects.
+    // When we use Arrays.sort() on an array of Car objects, then
+    // the array will be sorted based on year.
+
+    // Note that we are overriding the compareTo() method that is defined
+    // in the Comparable<T> interface defined in the Java API.
     
     @Override
-    public int compareTo( Car other ) {
+    public int compareTo( Car otherCar ) {
         
-        if( this.engineSize < other.engineSize )
+        if( this.year < otherCar.year )        // will result in ascending order
             return -1;
-        else if( this.engineSize == other.engineSize)
+        else if( this.year == otherCar.year)
             return 0;
-        else return 1;
+        else
+            return 1;
+
+        // The above logic could also be implemented as
+        // "  return this.year - otherCar.year; "
     }
-    
+
+    //TODO Modify this class so that Cars are sorted based on their Make field.
+    // Note that when using Comparable, only one ordering can be in place at any one time.
+
     @Override
     public String toString()
     {
         return getClass().getSimpleName() + "{" + "make=" + make 
-                + ", model=" + model + ", engineSize=" + engineSize + '}';
+                + ", model=" + model + ", year=" + this.year +", engineSize=" + engineSize + '}';
     }
 }
